7c3bd522829808153b5fe850e1205d4947d67058
amelcsfcm <amel.ensi@gmail.com>
2023-08-22 12:05:16 +0100

    change1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  
+  state = {
+    Person:{ 
+      fullName:"amel fatnassi"
+      , imgSrc:"https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=740&t=st=1692647197~exp=1692647797~hmac=96c7204ef58889fad894ffab017760af6281427e54ff650e808a93dd7d124920"
+      , profession:"Conseillère d'apprentissage"},
+   show:false 
+  };
+ 
+  render() {
+    return (
+      <div>
+        <button onClick={()=>{this.setState({show:!this.state.show})}}>toggles</button>
+      {
+        this.state.show==true?<div>
+          <h2>{this.state.Person.fullName}</h2>
+          <h2>{this.state.Person.profession}</h2>
+          <img src={this.state.Person.imgSrc}/>
+          </div>:null
+      }
+      </div>
+    )
+  }
 }
-
-export default App;
